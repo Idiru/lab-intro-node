615da50ba9159f7ae42a71f85d555a25b1eb1e06
Idir FOUQUET <fouquetidir@gmail.com>
2024-05-13 21:15:51 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort(function(a,b){
+      return a-b
+    })
+    this.length++
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!this.items.includes(pos)) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items.indexOf(pos)
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return Math.max(...this.items)
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("OutOfBounds");
+    } else {
+      return Math.min(...this.items)
+    }
+  }
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0 
+    } else {
+      return this.items.reduce((acc, curr) => acc + curr,
+      0,)
+    }
+    
+  }
+
+  avg() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+
+    } else {
+      const sum = this.items.reduce((acc, curr) => acc + curr,
+      0,)
+      return sum/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
